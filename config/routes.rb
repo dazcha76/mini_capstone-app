e33834ac5c2980e17161ebefbf8c8a3cf1cdce65
Danika <dazcha@hotmail.com>
2016-07-13 18:21:18 -0500

    RESTful pages complete and working

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post '/products' => 'products#create'
   get '/products/:id' => 'products#show'
   get '/products/:id/edit' => 'products#edit'
-  patch '/products/:id/edit' => 'products#update'
+  patch '/products/:id/' => 'products#update'
   delete '/products/:id' => 'products#destroy'
   
 
